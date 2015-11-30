08baee388044207f1b74bdb041619d24ab81b92f
Matt Faulkner <mnfaulk@gmail.com>
2015-11-30 13:06:10 -0800

    Initial project structure from angular-fullstack. Removes eol=lf from gitattributes

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('twoFactorPasswordApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
